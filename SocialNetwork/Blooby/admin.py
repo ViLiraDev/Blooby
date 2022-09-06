b68f57002f945427342bcfd88c089db898b49eaa
unknown <vilira2501@gmail.com>
2022-09-06 03:41:17 -0300

    Chat

--- SocialNetwork/Blooby/admin.py
+++ SocialNetwork/Blooby/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Profile, Post, Comment
-from chats.models import ChatMessage,Thread,ThreadManager
+from chats.models import ChatMessage,Thread
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
