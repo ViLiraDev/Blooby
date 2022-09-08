45fbd65042d47dbe6b1cb0bfe71db8fb1412c23b
unknown <vilira2501@gmail.com>
2022-09-07 21:32:45 -0300

    Heroku Test

--- SocialNetwork/SocialNetwork/settings.py
+++ SocialNetwork/SocialNetwork/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django-heroku
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
